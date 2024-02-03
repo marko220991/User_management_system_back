880a346bbbc7c55d21064a4c20ae54b8609e1447
Marko Ilic <19marko91.ilic@gmail.com>
2024-02-03 12:55:23 +0100

    Remove unused imports

--- src/main/java/com/webApp/User_Management_System/UserManagementSystemApplication.java
+++ src/main/java/com/webApp/User_Management_System/UserManagementSystemApplication.java
@@ -1,16 +1,11 @@
 package com.webApp.User_Management_System;
 
-import com.webApp.User_Management_System.model.User;
-import com.webApp.User_Management_System.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class UserManagementSystemApplication {
